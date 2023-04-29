36b97ba40454b7e95701f61cb18b9c8f140f0b74
BhargavM123 <80960862+BhargavM123@users.noreply.github.com>
2023-04-30 00:21:47 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,8 +1,6 @@
 import re
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-# import nltk
-# nltk.download('vader_lexicon')
 
 sentiments = SentimentIntensityAnalyzer()
 
